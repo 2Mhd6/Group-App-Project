1ff0f8175f71595a4b10d2ac0c1d703625857c91
Bushra <you@example.com>
2025-06-21 22:31:50 +0300

    my task completed

--- lib/ui/screens/main_screen/screens/orders/orders_screen.dart
+++ lib/ui/screens/main_screen/screens/orders/orders_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:group_app_project/shared/widgets/empty_space.dart';
-import 'package:group_app_project/theme/app_color.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:group_app_project/repositories/order/order.dart';
+import 'package:group_app_project/ui/screens/main_screen/screens/orders/bloc/orders_bloc.dart';
+import 'package:group_app_project/ui/screens/main_screen/screens/orders/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) => BlocProvider<OrdersBloc>(
+    // TODO Add the real userId
+    create: (context) {
+      final userId = '15a5c743-31bc-4d52-afda-f6f25faf5bed';
+      return OrdersBloc(ordersRepository: context.read<OrdersRepository>())
+          ..add(LoadOrdersEvent(userId: userId));
+    },
+    child: _OrdersScreenContent(),
+  );
+}
+
+class _OrdersScreenContent extends StatelessWidget {
+  const _OrdersScreenContent();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,53 +30,39 @@ class OrdersScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Orders', style: Theme.of(context).textTheme.displayLarge),
-          Expanded(
-            child: ListView.separated(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16.0),
-                  padding: EdgeInsets.all(16.0),
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    color: AppColor.secondaryAppColor,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          BlocBuilder<OrdersBloc, OrdersState>(
+            builder: (context, state) {
+              return Expanded(
+                child: switch (state) {
+                  OrdersInitial() => Container(),
 
-                    children: [
-                      Text(
-                        'Order title',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                      EmptySpace.sizeH24,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'data',
-                            style: Theme.of(context).textTheme.bodyMedium!
-                                .copyWith(color: Colors.grey),
+                  OrdersLoading() => Center(child: CircularProgressIndicator()),
+
+                  OrdersData(:final orders) =>
+                  //If empty, displays an empty message.
+                    orders.isEmpty
+                        ? Center(child: Text('Empty Orders'))
+                        : ListView.separated(
+                            itemCount: orders.length,
+                            itemBuilder: (context, index) {
+                              final order = orders[index];
+                              //If there are orders, displays them in a list.
+                              return OrderItemView(order: order);
+                            },
+                            separatorBuilder: (context, index) => Divider(),
                           ),
-                          Text(
-                            'data',
-                            style: Theme.of(context).textTheme.bodyMedium!
-                                .copyWith(color: Colors.grey),
-                          ),
-                          Text(
-                            'data',
-                            style: Theme.of(context).textTheme.bodyMedium!
-                                .copyWith(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
+
+                          //If there is an error, displays the error message.
+                  OrdersError(:final message) => Center(
+                    child: Text(
+                      message,
+                      style: Theme.of(context).textTheme.headlineLarge!
+                          .copyWith(color: Theme.of(context).colorScheme.error),
+                    ),
                   ),
-                );
-              },
-              separatorBuilder: (context, index) => Divider(),
-            ),
+                },
+              );
+            },
           ),
         ],
       ),
